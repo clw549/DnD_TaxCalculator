7f7b786a04e9f830847686f06e9f145e7bd2a5b4
kv422 <116920363+kv422@users.noreply.github.com>
2025-03-23 21:09:32 -0700

    Tax location

---
+++ build.bat
@@ -0,0 +1,2 @@
+docker build -t tax_service .
+docker run -p 5003:5003 tax_service
---
+++ tax_db.sql
@@ -0,0 +1,5 @@
+create table if not exists Tax(
+    tax_id INT PRIMARY KEY,
+    tax_amount INT,
+    tax_location VARCHAR(255)
+);
